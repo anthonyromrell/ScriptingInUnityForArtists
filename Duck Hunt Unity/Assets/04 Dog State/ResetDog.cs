8db7edfe0c583845edc6294c54546533c3839a99
Anthony Romrell <anthony@artisandream.com>
2014-12-02 14:06:35 -0700

    Completed dog state.

---
+++ Duck Hunt Unity/Assets/04 Dog State/ResetDog.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetDog : MonoBehaviour {
+
+	public Animator dogAnims;
+
+	void ResetTheDog () {
+		dogAnims.SetBool ("hit", false);
+		dogAnims.SetBool ("miss", false);
+	}
+}
