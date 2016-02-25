488ce7725324dac3817c4f72166020a8fff3b061
Anthony Romrell <anthony@artisandream.com>
2016-02-25 13:23:34 -0700

    Added Player Movement

---
+++ Duck Hunt Unity/Assets/Scripts/Intermediate/CharacterControl/MoveUsingArrowKeys.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MoveUsingArrowKeys : MonoBehaviour {
+
+	public static Action<float> MoveOnArrows;
+	public static Action<float> RotateOnArrows;
+
+	void Update () {
+		if (MoveOnArrows != null)
+			MoveOnArrows (Input.GetAxis ("Vertical"));
+
+		if (RotateOnArrows != null)
+			RotateOnArrows(Input.GetAxis ("Horizontal"));
+	}
+}
---
+++ Duck Hunt Unity/Assets/Scripts/Intermediate/CharacterControl/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Vector3 moveDirection;
+	public float speed = 10;
+	CharacterController cc;
+
+	void Start () {
+		cc = GetComponent<CharacterController> ();
+		MoveUsingArrowKeys.MoveOnArrows += Move;
+		//MoveUsingButtons.Forward += Move;
+
+		MoveUsingArrowKeys.RotateOnArrows += Rotate;
+		//MoveUsingButtons.Rotate += Rotate;
+	}
+
+	void Rotate (float _rotateInY)
+	{
+		transform.Rotate (0, _rotateInY, 0);
+	}
+
+	void Move (float _moveInZ) {
+		moveDirection.z = _moveInZ;
+		moveDirection = transform.TransformDirection (moveDirection*speed*Time.deltaTime);
+		cc.Move (moveDirection);
+	}
+}
