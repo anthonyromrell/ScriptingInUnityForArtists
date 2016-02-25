50afff0eea5973a81f771eeec1e6a4f55acc5be3
Anthony Romrell <anthony@artisandream.com>
2016-02-25 10:42:35 -0700

    Added AI.

---
+++ Duck Hunt Unity/Assets/Scripts/Advanced/AI/RandomAiPatrol.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class RandomAiPatrol : MonoBehaviour {
+
+		public Transform[] patrolPoints;
+		NavMeshAgent enemyAgent;
+		int currentPatrolPoint = 0;
+
+		Action CanPatrol;
+
+		void Patrolling() {
+				if (patrolPoints.Length == 0)
+						return;
+				enemyAgent.destination = patrolPoints [currentPatrolPoint].position;
+
+				currentPatrolPoint = UnityEngine.Random.Range (0, patrolPoints.Length-1);
+		}
+				
+		void Update () {
+				if (enemyAgent.remainingDistance < 0.5f) {
+						if(CanPatrol != null)
+								CanPatrol();
+				}
+		}
+
+		void Start () {
+				enemyAgent = GetComponent<NavMeshAgent> ();
+				CanPatrol += Patrolling;
+		}
+}
