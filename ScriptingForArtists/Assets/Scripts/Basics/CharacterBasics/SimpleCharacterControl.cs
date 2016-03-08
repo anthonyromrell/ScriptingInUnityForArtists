d8b7c83db6b277dfd8dc1890d30022df4b22fc75
Anthony Romrell <anthony@artisandream.com>
2016-03-08 13:22:24 -0700

    updated.

---
+++ ScriptingForArtists/Assets/Scripts/Basics/CharacterBasics/SimpleCharacterControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCharacterControl : MonoBehaviour {
+
+	private CharacterController character;
+	private Vector3 moveVector;
+	public float moveSpeed = 5;
+	public float rotateSpeed = 2;
+
+	void Start () {
+		character = GetComponent<CharacterController> ();
+	}
+
+	void Rotate (float _myInput) 
+	{
+		transform.Rotate (0, _myInput*rotateSpeed, 0);
+	}
+
+	void Move (float _myInput) 
+	{
+		moveVector.x = _myInput;
+		moveVector = transform.TransformDirection (moveVector*moveSpeed*Time.deltaTime);
+		character.Move (moveVector);
+	}
+	
+	void Update () {
+		Move (Input.GetAxis ("Vertical"));
+		Rotate (Input.GetAxis ("Horizontal"));
+	}
+}
