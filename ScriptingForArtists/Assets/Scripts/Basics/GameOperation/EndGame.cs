4789ef7b8355a9cc7235e6035861bd6f1cff20ab
Anthony Romrell <anthony@artisandream.com>
2016-03-22 11:17:55 -0600

    Starting and ending a game

---
+++ ScriptingForArtists/Assets/Scripts/Basics/GameOperation/EndGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndGame : MonoBehaviour {
+
+	float health = 1;
+	public Transform healthBar;
+	Vector3 healthScale;
+
+	IEnumerator EndThisGame ()
+	{
+		yield return new WaitForSeconds (1);
+		Application.LoadLevel ("EndGame");
+	}
+
+	// Use this for initialization
+	void OnMouseDown () {
+		health -= 0.1f;
+		healthScale.x = health;
+		healthBar.localScale = healthScale;
+
+		if (health > 0)
+			return;
+
+		StartCoroutine (EndThisGame ());
+		healthBar.gameObject.SetActive (false);
+	}
+
+	void Start () {
+		healthScale.y = 1;
+		healthScale.z = 1;
+	}
+}
---
+++ ScriptingForArtists/Assets/Scripts/Basics/GameOperation/StartGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public string GameLevel = "GameLevel";
+
+	public void Play ()
+	{
+		Application.LoadLevel (GameLevel);
+	}
+}
