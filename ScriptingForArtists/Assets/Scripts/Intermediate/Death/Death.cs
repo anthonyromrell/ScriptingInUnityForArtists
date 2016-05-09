7eb036e4da7c131849f5d7f5d05874347628ab25
Anthony Romrell <anthony@artisandream.com>
2016-05-09 10:56:31 -0600

    Updated Death Scripts

---
+++ ScriptingForArtists/Assets/Scripts/Intermediate/Death/Death.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Death : MonoBehaviour {
+
+	public static Action GameOver;
+
+	void OnTriggerEnter () {
+		if (GameOver != null)
+			GameOver ();
+
+		gameObject.SetActive (false);
+	}
+
+}
---
+++ ScriptingForArtists/Assets/Scripts/Intermediate/Death/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	void StopGame ()
+	{
+		print ("Game Over");
+		Death.GameOver -= StopGame;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Death.GameOver += StopGame;	
+	}
+
+}
