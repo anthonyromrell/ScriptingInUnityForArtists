0e8e99132bd6993def8ca28b534156681be81e62
Anthony Romrell <anthony@artisandream.com>
2016-05-23 11:08:09 -0600

    Syntax Lecture

---
+++ ScriptingForArtists/Assets/Scripts/Basics/Syntax/Syntax.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour {
+
+	int myInt = 5;
+
+	int MyInt (int _i, int _j) {
+		return _i + _j;
+	}
+
+	int MyIntBy2 (int _i) {
+		int _k = (_i + 10)*2;
+		return _k;
+	}
+
+	int MyIntNoArgs () {
+		return 8 + 9 * 10;
+	}
+
+	void Start () {
+		print (myInt);
+		myInt = MyIntBy2 (10);
+		print (myInt);
+		myInt = MyIntNoArgs();
+		print (myInt);
+		myInt = MyInt (45, 35);
+		print (myInt);
+	}
+}
