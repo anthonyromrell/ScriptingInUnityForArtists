1f736a17e8d88ae83d19f3778c99940aaa293177
Anthony Romrell <anthony@artisandream.com>
2016-07-11 10:17:20 -0600

    FoorLoops

---
+++ ScriptingForArtists/Assets/Scripts/Basics/Loops/ForLoops.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoops : MonoBehaviour {
+
+	public int[] playerScores;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < playerScores.Length; i++) {
+			playerScores[i] += 10;
+			playerScores[i] += 10;
+			print (playerScores[i]);
+			print (i);
+		}
+	}
+}
---
+++ ScriptingForArtists/Assets/Scripts/Basics/Loops/Foreach.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foreach : MonoBehaviour {
+
+	public string[] playerNames;
+	public string search;
+
+	// Use this for initialization
+	void Start () {
+		foreach (string _name in playerNames) {
+			if(_name == search)
+				print (_name);
+		}
+	}
+}
