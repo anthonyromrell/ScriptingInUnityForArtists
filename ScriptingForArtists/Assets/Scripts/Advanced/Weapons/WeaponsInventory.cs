ba52b9ec17c09fc9946ae2089b4b2b85f53173cc
Anthony Romrell <anthony@artisandream.com>
2016-07-13 11:33:03 -0600

    Final on feature.

---
+++ ScriptingForArtists/Assets/Scripts/Advanced/Weapons/WeaponsInventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using ArtisanDream;
+
+namespace ArtisanDream {
+public class WeaponsInventory : MonoBehaviour {
+
+		public List<WeaponBase> myWeapons = new List<WeaponBase>();
+
+		void Update () {
+			if (Input.GetKeyUp (KeyCode.R) && myWeapons.Count > 0) {
+				myWeapons[0].gameObject.SetActive(true);
+				myWeapons.RemoveAt (0);
+			}
+
+			if (Input.GetKeyUp (KeyCode.F) && myWeapons.Count > 0 && myWeapons [0].ammoCount > 0) {
+				print ("BANG");
+				myWeapons [0].ammoCount--;
+			} else if (Input.GetKeyUp (KeyCode.F) && myWeapons.Count > 0) {
+				myWeapons.RemoveAt (0);
+			}
+		}
+
+	}
+}
